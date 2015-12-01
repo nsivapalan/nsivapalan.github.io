523ba3c8963f09e0e1d6280e1e6650b8488ba5f3
Nimalen Sivapalan <nsivapalan@berkeley.edu>
2015-12-01 10:39:05 -0800

    update js and add scroll to top button

--- assets/script.js
+++ assets/script.js
@@ -4,4 +4,16 @@ $(document).ready(function() {
         $('.contactForm').fadeIn(1500);
     });
 
+    $(window).scroll(function(){
+        if ($(window).scrollTop() + $(window).height() ==
+            $(document).height()) {
+            $('.top').show();
+        } else {
+            $('.top').hide();
+        }
+    });
+
+    $('.top').click(function() {
+       $('html, body').animate({scrollTop: '0px'});
+    });
 });
