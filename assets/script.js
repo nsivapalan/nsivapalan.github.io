0b9b8f799f096596fba4f894df762325dfb9ef59
Nimalen Sivapalan <nsivapalan@berkeley.edu>
2015-12-01 19:03:04 -0800

    version 1 of website complete

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     $('#click').click(function() {
         $('#click').hide();
-        $('.contactForm').fadeIn(1500);
+        $('.contactForm').fadeIn(500);
     });
 
     $(window).scroll(function(){
-        if ($(window).scrollTop() + $(window).height() ==
-            $(document).height()) {
-            $('.top').show();
+        if ($(window).scrollTop() + $(window).height() >
+            $(document).height() - 35) {
+            $('.top').fadeIn(500);
         } else {
             $('.top').hide();
         }
@@ -15,5 +15,31 @@ $(document).ready(function() {
 
     $('.top').click(function() {
        $('html, body').animate({scrollTop: '0px'});
+    });
+
+    $('.icon').hover(function() {
+        $('#hobbiesIntro').hide();
+    });
+
+    $('.reading').hover(function() {
+        $('.icon').removeClass('showing');
+        $('.description').hide();
+        $('#readingDescription').show();
+        $(this).addClass('showing');
+    });
+
+    $('.coding').hover(function() {
+        $('.icon').removeClass('showing');
+        $('.description').hide();
+        $('#codingDescription').show();
+        $(this).addClass('showing');
+
+    });
+
+    $('.travel').hover(function() {
+        $('.icon').removeClass('showing');
+        $('.description').hide();
+        $('#travelDescription').show();
+        $(this).addClass('showing');
     });
 });
