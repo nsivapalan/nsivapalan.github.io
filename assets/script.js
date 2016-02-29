742250359fdcf6d95a0814f28a8ffd4d03784606
Nimalen Sivapalan <nsivapalan@berkeley.edu>
2016-02-28 22:10:06 -0800

    add google analytics and minor changes

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-    $('#click').click(function() {
-        $('#click').hide();
-        $('.contactForm').fadeIn(500);
-    });
 
     $(window).scroll(function(){
         if ($(window).scrollTop() + $(window).height() >
