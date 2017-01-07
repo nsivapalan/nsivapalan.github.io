e4ded416a72fd32100aa33774ae18dcabf452fe0
Nimalen Sivapalan <nsivapalan@berkeley.edu>
2017-01-06 19:18:44 -0800

    update scriptand scroll to top

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,7 @@
 $(document).ready(function() {
 
-    $(window).scroll(function(){
-        if ($(window).scrollTop() + $(window).height() >
-            $(document).height() - 35) {
-            $('.top').fadeIn(500);
-        } else {
-            $('.top').hide();
-        }
-    });
-
     $('.top').click(function() {
        $('html, body').animate({scrollTop: '0px'});
     });
-
-    $('.icon').hover(function() {
-        $('#hobbiesIntro').hide();
-    });
-
-    $('.reading').hover(function() {
-        $('.icon').removeClass('showing');
-        $('.description').hide();
-        $('#readingDescription').show();
-        $(this).addClass('showing');
-    });
-
-    $('.coding').hover(function() {
-        $('.icon').removeClass('showing');
-        $('.description').hide();
-        $('#codingDescription').show();
-        $(this).addClass('showing');
-
-    });
-
-    $('.travel').hover(function() {
-        $('.icon').removeClass('showing');
-        $('.description').hide();
-        $('#travelDescription').show();
-        $(this).addClass('showing');
-    });
+    
 });
